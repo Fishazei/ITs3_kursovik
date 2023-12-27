479a5f35e9f618be63a17e19eaa9309d6986a062
Fishazei <fishazei@mail.ru>
2023-12-28 01:02:59 +0300

    v0.8.1

--- IT_3S_Kursovik/obj/Debug/net8.0-windows/IT_3S_Kursovik_wffaglvo_wpftmp.AssemblyInfo.cs
+++ IT_3S_Kursovik/obj/Debug/net8.0-windows/IT_3S_Kursovik_wffaglvo_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT_3S_Kursovik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494fa4078509c31d19bb2e12d4bdbfeebcee9d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT_3S_Kursovik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT_3S_Kursovik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
